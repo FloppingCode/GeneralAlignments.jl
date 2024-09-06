e6593991a9d7560c694a9a6119e6e1c87a105ccb
FloppingCode <86307609+FloppingCode@users.noreply.github.com>
2024-09-06 16:26:46 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.51

---
+++ src/GeneralAlignments.jl
@@ -0,0 +1,5 @@
+module GeneralAlignments
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using GeneralAlignments
+using Test
+
+@testset "GeneralAlignments.jl" begin
+    # Write your tests here.
+end
